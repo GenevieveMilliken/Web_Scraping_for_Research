2978c3e0b06a9af734d9e4030353d987a9a58671
Genevieve Milliken <gmillike@pratt.edu>
2019-09-29 20:23:14 -0400

    Add WIP Code4Lib Script

---
+++ Code4Lib/Code4Lib.py
@@ -0,0 +1,135 @@
+# This script captures the following content from Code4Lib:
+# article URL, title, author and abstract
+# Issue URL, number, and date
+
+# import modules
+from bs4 import BeautifulSoup
+import requests
+import json
+
+# create empty list
+all_my_data = []
+
+# assign issue archive URL as variable
+url = 'https://journal.code4lib.org/issues'
+
+# request URL and make soup object
+archives = requests.get(url)
+archives_html = archives.text 
+soup = BeautifulSoup(archives_html, 'html.parser')
+
+# locate HTML section containing each article within each issue
+articles = soup.find_all('h3', attrs = {'class': 'articletitle'})
+
+my_data = {
+		"article_URL": None,
+		"title": None, 
+		"author": None,
+		"abstract": None,
+		"issue_URL": None,
+		"issue_number": None, 
+		"issue_date": None,
+	}
+
+
+
+# create for loop to loop through each article
+# set up dictionary for content categories 
+for article in articles: 
+
+
+	print('-----------------------')
+
+	# get Article URL
+	article_link = article.find('a')
+	article_link_href = article_link['href']
+	my_data['article_URL'] = article_link_href
+	
+
+	# get article title
+	article_title = article_link.text
+	my_data['title'] = article_title 
+	
+
+	# turn each article into a soup item
+	article_item = requests.get(article_link_href)
+	article_html = article_item.text 
+	article_soup = BeautifulSoup(article_html, 'html.parser')
+	
+
+	# get article abstract
+	abstract = article_soup.find('div', attrs= {'class': 'abstract'})
+	abstract = abstract.text
+	my_data['abstract'] = abstract
+
+	# get author(s) by creating a for loop
+	authors = article_soup.find_all('meta', attrs={'name': 'citation_author'})
+
+	author_list = []
+
+	for author in authors:
+		each_author = author['content']
+		author_list.append(each_author)
+		my_data['author'] = author_list
+
+	#get issue URL
+	issue_link = article_soup.find('p', attrs={'id': 'issueDesignation'})
+	issue_link = issue_link.find('a')
+	issue_link = issue_link['href']
+	my_data['issue_URL'] = issue_link
+
+	# get issue number
+	issue_number = article_soup.find('meta', attrs={'name': 'citation_issue'})
+	issue_number = issue_number['content']
+	my_data['issue_number'] = issue_number
+
+	# get issue date
+	issue_date = article_soup.find('meta', attrs={'name': 'citation_publication_date'})
+	issue_date = issue_date['content']
+	my_data['issue_date'] = issue_date
+
+	print(my_data)
+	
+		
+	
+
+
+
+
+	
+
+
+
+	
+	
+
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
